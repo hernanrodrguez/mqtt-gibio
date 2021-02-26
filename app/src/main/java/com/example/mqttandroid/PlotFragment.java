41a748c6a2228efa84b10d283ca35b5f96fa579e
NahueGrimaudo <78801109+NahueGrimaudo@users.noreply.github.com>
2021-02-26 16:32:57 -0300

    Coding the graph

--- app/src/main/java/com/example/mqttandroid/PlotFragment.java
+++ app/src/main/java/com/example/mqttandroid/PlotFragment.java
@@ -4,10 +4,19 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +29,20 @@ public class PlotFragment extends Fragment {
 
     private String data;
     private static final String DATA_KEY = "Data";
+
+
+    //add PointsGraphSeries of DataPoint type
+    PointsGraphSeries<DataPoint> xySeries;
+
+    private Button btnAddPt;
+
+    private EditText mX,mY;
+
+    GraphView mScatterPlot;
+
+    //make xyValueArray global
+    private ArrayList<XYpoints> xyValueArray;
+
 
     public PlotFragment() {
         // Required empty public constructor
@@ -45,9 +68,156 @@ public class PlotFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_plot, container, false);
 
+        btnAddPt = (Button) view.findViewById(R.id.btnAddPt);
+        mX = (EditText) view.findViewById(R.id.numX);
+        mY = (EditText) view.findViewById(R.id.numY);
+        mScatterPlot = (GraphView) view.findViewById(R.id.scatterPlot);
+        xyValueArray = new ArrayList<>();
+
+
+
+
+
+
         tvDummy = view.findViewById(R.id.tvDummy);
         tvDummy.setText(data);
 
+        init();
+
         return view;
     }
+
+
+    private void init(){
+        //declare the xySeries Object
+        xySeries = new PointsGraphSeries<>();
+
+        btnAddPt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!mX.getText().toString().equals("") && !mY.getText().toString().equals("") ){
+                    double x = Double.parseDouble(mX.getText().toString());
+                    double y = Double.parseDouble(mY.getText().toString());
+                    Log.d(TAG, "onClick: Adding a new point. (x,y): (" + x + "," + y + ")" );
+                    xyValueArray.add(new XYpoints(x,y));
+                    init();
+                }else {
+                    toastMessage("You must fill out both fields!");
+                }
+            }
+        });
+
+        //little bit of exception handling for if there is no data.
+        if(xyValueArray.size() != 0){
+            createScatterPlot();
+        }else{
+            Log.d(TAG, "onCreate: No data to plot.");
+        }
+    }
+
+
+    private void createScatterPlot() {
+        Log.d(TAG, "createScatterPlot: Creating scatter plot.");
+
+        //sort the array of xy values
+        xyValueArray = sortArray(xyValueArray);
+
+        //add the data to the series
+        for(int i = 0;i <xyValueArray.size(); i++){
+            try{
+                double x = xyValueArray.get(i).getX();
+                double y = xyValueArray.get(i).getY();
+                xySeries.appendData(new DataPoint(x,y),true, 1000);
+            }catch (IllegalArgumentException e){
+                Log.e(TAG, "createScatterPlot: IllegalArgumentException: " + e.getMessage() );
+            }
+        }
+
+        //set some properties
+        xySeries.setShape(PointsGraphSeries.Shape.RECTANGLE);
+        xySeries.setColor(Color.BLUE);
+        xySeries.setSize(20f);
+
+        //set Scrollable and Scaleable
+        mScatterPlot.getViewport().setScalable(true);
+        mScatterPlot.getViewport().setScalableY(true);
+        mScatterPlot.getViewport().setScrollable(true);
+        mScatterPlot.getViewport().setScrollableY(true);
+
+        //set manual x bounds
+        mScatterPlot.getViewport().setYAxisBoundsManual(true);
+        mScatterPlot.getViewport().setMaxY(150);
+        mScatterPlot.getViewport().setMinY(-150);
+
+        //set manual y bounds
+        mScatterPlot.getViewport().setXAxisBoundsManual(true);
+        mScatterPlot.getViewport().setMaxX(150);
+        mScatterPlot.getViewport().setMinX(-150);
+
+        mScatterPlot.addSeries(xySeries);
+    }
+
+    /**
+     * Sorts an ArrayList<XYValue> with respect to the x values.
+     * @param array
+     * @return
+     */
+    private ArrayList<XYValue> sortArray(ArrayList<XYValue> array){
+        /*
+        //Sorts the xyValues in Ascending order to prepare them for the PointsGraphSeries<DataSet>
+         */
+        int factor = Integer.parseInt(String.valueOf(Math.round(Math.pow(array.size(),2))));
+        int m = array.size() - 1;
+        int count = 0;
+        Log.d(TAG, "sortArray: Sorting the XYArray.");
+
+
+        while (true) {
+            m--;
+            if (m <= 0) {
+                m = array.size() - 1;
+            }
+            Log.d(TAG, "sortArray: m = " + m);
+            try {
+                //print out the y entrys so we know what the order looks like
+                //Log.d(TAG, "sortArray: Order:");
+                //for(int n = 0;n < array.size();n++){
+                //Log.d(TAG, "sortArray: " + array.get(n).getY());
+                //}
+                double tempY = array.get(m - 1).getY();
+                double tempX = array.get(m - 1).getX();
+                if (tempX > array.get(m).getX()) {
+                    array.get(m - 1).setY(array.get(m).getY());
+                    array.get(m).setY(tempY);
+                    array.get(m - 1).setX(array.get(m).getX());
+                    array.get(m).setX(tempX);
+                } else if (tempX == array.get(m).getX()) {
+                    count++;
+                    Log.d(TAG, "sortArray: count = " + count);
+                } else if (array.get(m).getX() > array.get(m - 1).getX()) {
+                    count++;
+                    Log.d(TAG, "sortArray: count = " + count);
+                }
+                //break when factorial is done
+                if (count == factor) {
+                    break;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                Log.e(TAG, "sortArray: ArrayIndexOutOfBoundsException. Need more than 1 data point to create Plot." +
+                        e.getMessage());
+                break;
+            }
+        }
+        return array;
+    }
+
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
